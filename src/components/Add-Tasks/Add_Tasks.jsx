c9729cde812e9523975df21675eece3c7c7a2af8
Aakash-Developer <aakash20899macwan@gmail.com>
2023-07-11 18:39:01 +0530

    11-07-update

--- src/components/Add-Tasks/Add_Tasks.jsx
+++ src/components/Add-Tasks/Add_Tasks.jsx
@@ -1,15 +1,15 @@
 import './add_task.css';
 export default function Add_Tasks() {
   return (
-    <div className='my-5'>
-    <form>
+    <div className='my-3 my-md-5'>
+    <form className="container">
          <div class="row align-items-start">
           <div class="col-12 col-md-7 col-sm-12  px-1">
           <div class="input-group mb-3">
           <input type="text" class="form-control" placeholder='New Task'/>
         </div>
           </div>
-          <div class=" col-6 col-md-3 col-sm-6 px-1">
+          <div class="col-6 col-md-3 col-sm-6 px-1">
           <select class="form-select w-100" aria-label="Default select example">
   <option value="2">Completed</option>
   <option value="3">Pending</option>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 export default function Header() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  <div class="container-fluid px-md-5 ">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand text-uppercase fw-bolder" href="#">Todo-List-App</a>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
